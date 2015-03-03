d136883959afc97e27acfda19e2d8b09a1edb111
zhangxuej <957168943@qq.com>
2015-03-03 11:22:44 +0800

    Change-Id: I1aaaa92c017dff287395216db7a66fd8dccfc802

---
+++ src/main/java/com/baobaotao/domain/BaseDomain.java
@@ -0,0 +1,22 @@
+package com.baobaotao.domain;
+
+import java.io.Serializable;
+
+import org.apache.commons.lang.builder.ToStringBuilder;
+
+/**
+ * 
+ *<br><b>类描述:</b>
+ *<pre>所示PO的父类</pre>
+ *@see
+ *@since
+ */
+@SuppressWarnings("serial")
+public class BaseDomain implements Serializable {
+	
+	@Override
+	public String toString() {
+		return ToStringBuilder.reflectionToString(this);
+	}
+
+}
