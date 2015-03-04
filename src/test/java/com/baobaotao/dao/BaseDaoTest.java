2c7e771f243ce89fe05389401e282df48a786c7b
zhangxuej <957168943@qq.com>
2015-03-04 17:06:37 +0800

    Change-Id: If89ea26bdcfeea431b7535a80d515f99bf99931b

--- src/test/java/com/baobaotao/dao/BaseDaoTest.java
+++ src/test/java/com/baobaotao/dao/BaseDaoTest.java
@@ -1,15 +1,20 @@
 package com.baobaotao.dao;
 
+import java.io.File;
+
+import junit.framework.Assert;
+
 import org.junit.Test;
 import org.unitils.UnitilsJUnit4;
 import org.unitils.spring.annotation.SpringApplicationContext;
 
-@SpringApplicationContext({ "classpath:/baobaotao-dao.xml" })
+@SpringApplicationContext({ "classpath:/resources/baobaotao-dao.xml" })
 public class BaseDaoTest extends UnitilsJUnit4 {
 	
 	@Test
 	public void test1() {
-		System.out.println(1);
+		File file = new File("src/main/java/resources/baobaotao-dao.xml");
+		Assert.assertEquals(file.exists(), true);
 	}
 
 }
