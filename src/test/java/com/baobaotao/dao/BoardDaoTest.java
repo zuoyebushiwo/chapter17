13248c43e1acc912f1a59bae8a2f776e3d119719
zhangxuej <957168943@qq.com>
2015-03-06 09:34:35 +0800

    Change-Id: I2543038bb95b5affe36b05dc29cad9bbd1a2e9ab

--- src/test/java/com/baobaotao/dao/BoardDaoTest.java
+++ src/test/java/com/baobaotao/dao/BoardDaoTest.java
@@ -1,8 +1,13 @@
 package com.baobaotao.dao;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+
 import java.util.List;
 
+import org.hamcrest.Matchers;
 import org.junit.Test;
+import org.unitils.dbunit.annotation.DataSet;
 import org.unitils.dbunit.annotation.ExpectedDataSet;
 import org.unitils.spring.annotation.SpringBean;
 
@@ -30,6 +35,27 @@ public class BoardDaoTest extends BaseDaoTest {
 		for (Board board : boards) {
 			boardDao.save(board);
 		}
+	}
+
+	/**
+	 * 删除一个板块
+	 */
+	@Test
+	@DataSet("BaobaoTao.Boards.xls")
+	// 准备数据
+	@ExpectedDataSet("BaobaoTao.ExpectedBoards.xls")
+	public void removeBoard() {
+		Board board = boardDao.get(7);
+		boardDao.remove(board);
+	}
+
+	@Test
+	@DataSet("BaobaoTao.Boards.xls")
+	// 准备数据
+	public void getBoard() {
+		Board board = boardDao.load(1);
+		assertNotNull(board);
+		assertThat(board.getBoardName(), Matchers.containsString("育儿"));
 	}
 
 }
