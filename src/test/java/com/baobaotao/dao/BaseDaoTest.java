97af6ce1e666690c2cdbc565875ffec4d3f23fec
zhangxuej <957168943@qq.com>
2015-03-08 22:37:55 +0800

    Change-Id: I4aaab47a9107c4095d760426ea7c72b48bf7e6bf

--- src/test/java/com/baobaotao/dao/BaseDaoTest.java
+++ src/test/java/com/baobaotao/dao/BaseDaoTest.java
@@ -8,15 +8,15 @@ import org.junit.Test;
 import org.unitils.UnitilsJUnit4;
 import org.unitils.spring.annotation.SpringApplicationContext;
 
-@SpringApplicationContext({ "classpath:/resources/baobaotao-dao.xml" })
+@SpringApplicationContext({ "classpath:baobaotao-dao.xml" })
 public class BaseDaoTest extends UnitilsJUnit4 {
 	
 	@Test
 	public void test1() {
-		File temp = new File("classpath:/resources/baobaotao-dao.xml");
+		File temp = new File("classpath:baobaotao-dao.xml");
 		System.out.println(temp.exists());
 		System.out.println(this.getClass().getClassLoader().getResource(""));
-		File file = new File("src/main/java/resources/baobaotao-dao.xml");
+		File file = new File("src/main/resources/baobaotao-dao.xml");
 		Assert.assertEquals(file.exists(), true);
 	}
 
