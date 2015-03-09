217e7979740983b45195d44a724e362873f406ba
zhangxuej <957168943@qq.com>
2015-03-09 10:11:12 +0800

    Change-Id: I80a2d2390716fe621d4c3967398f1d6aa53dc666

---
+++ src/test/java/com/baobaotao/web/controller/BaseWebTest.java
@@ -0,0 +1,34 @@
+package com.baobaotao.web.controller;
+
+import org.junit.Before;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.web.servlet.mvc.annotation.AnnotationMethodHandlerAdapter;
+import org.unitils.UnitilsJUnit4;
+import org.unitils.spring.annotation.SpringApplicationContext;
+import org.unitils.spring.annotation.SpringBeanByType;
+
+@SpringApplicationContext({ "classpath:/applicationContext.xml",
+		"file:src/main/webapp/WEB-INF/baobaotao-servlet.xml" })
+public class BaseWebTest extends UnitilsJUnit4 {
+
+	// 根据类型注入Bean
+	@SpringBeanByType
+	public AnnotationMethodHandlerAdapter handlerAdapter;
+
+	// 声明Request与Response模拟对象
+	public MockHttpServletRequest request;
+	public MockHttpServletResponse response;
+	public MockHttpSession session;
+
+	// 执行测试前先初始模拟对象
+	@Before
+	public void before() {
+		request = new MockHttpServletRequest();
+		response = new MockHttpServletResponse();
+		session = new MockHttpSession();
+		request.setCharacterEncoding("UTF-8");
+	}
+	
+}
